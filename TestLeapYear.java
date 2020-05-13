e09eec42b724e5d7c277141f7d5165334504d3c5
Swapnil Aher <33717709+swapi17@users.noreply.github.com>
2020-05-13 15:02:33 +0530

    initial commit for test file

---
+++ TestLeapYear.java
@@ -0,0 +1,23 @@
+package leapyear;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLeapYear {
+
+	private LeapYear leapYear;
+
+	@Before
+	public void setUp() throws Exception {
+		leapYear = new LeapYear();
+	}
+
+	@Test
+	public void TestIsLeap() {
+		
+	}
+
+		
+}
